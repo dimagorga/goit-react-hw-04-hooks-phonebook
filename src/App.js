9c263b621837c50a3231d5016a8b53be91b7f80e
Dzmitry Harhun <dimagorga@gmail.com>
2021-09-07 14:43:21 +0300

    hooks add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import ContactForm from "./Components/ContactForm/ContactForm";
+import ContactsList from "./Components/ContactsList/ContactsList";
+import Filter from "./Components/Filter/Filter";
+import "./App.css";
 
 function App() {
+  const [contacts, setContacts] = useState([
+    ...JSON.parse(localStorage.getItem("contacts")),
+  ]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    window.localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (nextContact) => {
+    const sameName = contacts.some(
+      (contact) =>
+        contact.name.toLocaleLowerCase() ===
+        nextContact.name.toLocaleLowerCase()
+    );
+    if (sameName) {
+      alert(`${nextContact.name} is already in contacts`);
+    } else {
+      setContacts((prevState) => [nextContact, ...prevState]);
+    }
+  };
+
+  const handleRemove = (id) => {
+    setContacts((prevState) => {
+      return prevState.filter((contact) => contact.id !== id);
+    });
+  };
+
+  const changeFilter = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter((contact) => {
+      return contact.name.toLowerCase().includes(normalizedFilter);
+    });
+  };
+
+  const visibleContacts = getVisibleContacts();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="mainTitle">Phonebook</h1>
+      <ContactForm handleSubmit={addContact} />
+      <h2 className="contactsTitle">Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactsList
+        contactsList={visibleContacts}
+        handleRemove={handleRemove}
+      />
     </div>
   );
 }
